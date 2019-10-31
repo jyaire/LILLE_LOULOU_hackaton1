1011673db85bc63ccad000d09006da86367d2fa3
Sophie Fonteneau <sophie.fonteneau@outlook.fr>
2019-10-31 10:55:10 +0100

    Création du deuxième page choix photos, mis à jour du css

--- src/Controller/TuesController.php
+++ src/Controller/TuesController.php
@@ -3,18 +3,15 @@
 
 namespace App\Controller;
 
-
-
 class TuesController extends AbstractController
-
 {
     public function photos()
-{
-    return $this->twig->render('choose/tuesplus.html.twig');
-}
-public function photos2()
-{
-    return $this->twig->render('choose/tuesplus2.html.twig');
-}
+    {
+        return $this->twig->render('choose/tuesplus.html.twig');
+    }
 
+    public function photos2()
+    {
+        return $this->twig->render('choose/tuesplus2.html.twig');
+    }
 }
